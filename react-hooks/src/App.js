7d4a51c85d319be6b669e00717f5eb06edae008a
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-02 17:59:12 +0300

    feat: add castom hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,17 +1,63 @@
-import React from 'react'
-import Main from './Main'
-import Alert from './alert/Alert'
-import { AlertProvider } from './alert/AlertContext'
+import React, { useEffect, useState } from 'react'
+
+function useLogger(value) {
+  useEffect( () => {
+    console.log('Value changed: ', value);
+  }, [value])
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const clear = () => setValue('')
+
+  return {
+    // value, onChange
+    bind: {value, onChange},
+    value,
+    clear
+  }
+}
 
 function App() {
 
+  // const [name, setName] = useState('')
+  // const [lastName, setLastName] = useState('')
+
+  // const changeHandler = (event) => {
+  //   setName(event.target.value)
+  // }
+
+  // const changeHandlerLastName = (event) => {
+  //   setLastName(event.target.value)
+  // }
+  // useLogger(name)
+
+  const input = useInput('')
+  // const lastName = useInput('')
+
+  useLogger(input.value)
+
   return (
-      <AlertProvider>
-        <div className={'container pt-3'}>
-          <Alert />
-          <Main />
-        </div>
-      </AlertProvider>
+      <div className={'container tt-3'}>
+        {/* <input type='text' value={name} onChange={changeHandler}/> */}
+        {/* <input type='text' value={input.value} onChange={input.onChange}/> */}
+        {/* <input type='text' {...input}/> */}
+        <input type='text' {...input.bind} />
+        {/* <input type='text' {...lastName}/> */}
+        {/* <input type='text' value={lastName} onChange={changeHandlerLastName}/> */}
+        {/* <hr /> */}
+        {/* <h2>{name}</h2> */}
+        {/* <h2>{name} {lastName}</h2> */}
+        <h2>{input.value}</h2>
+        {/* <h2>{lastName.value}</h2> */}
+
+        <button className={'btn btn-warning'} onClick={ () => input.clear()}>Очистить</button>
+      </div>
   )
 }
 
