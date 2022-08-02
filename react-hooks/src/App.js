92a7f5860323979301bb115782bd53d38db7d69b
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-02 16:16:58 +0300

    feat: learne useContext

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,27 +1,18 @@
-import React, {useState, useCallback} from 'react'
-import ItemsList from './itemsList'
+import React from 'react'
+import Main from './Main'
+import Alert from './alert/Alert'
+import { AlertProvider } from './alert/AlertContext'
 
 function App() {
-  const [colored, setColored] = useState(false)
-  const [count, setCount] = useState(1)
-
-  const styles = {
-      color: colored ? 'darkred' : 'black'
-  }
-
-  const generateItemsFromAPI = useCallback( () => {
-    return new Array(count).fill('').map( (_, i) => `Элемент ${i + 1}`)
-  }, [count] )
 
   return (
-    <div>
-      <h1 style={styles}>Количество элементов {count}</h1>
-      <button className='btn btn-success' onClick={ () => setCount( prev => prev + 1)}>Добавить</button>
-      <button className='btn btn-warning' onClick={ () => setColored( prev => !prev)}>Изменить</button>
-
-      <ItemsList getItems={generateItemsFromAPI}/>
-    </div>
+      <AlertProvider>
+        <div className={'container pt-3'}>
+          <Alert />
+          <Main />
+        </div>
+      </AlertProvider>
   )
 }
 
-export default App;
+export default App
---
+++ react-hooks/src/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import {useAlertToggle} from './alert/AlertContext'
+import {useAlert} from './alert/AlertContext'
+
+export default function Main() {
+
+    const {toggle} = useAlert()
+
+    return (
+        <>
+            <h1>Привет в примере с Context</h1>
+            <button onClick={toggle} className={'btn btn-success'}>Показать alert</button>
+        </>
+    )
+}
+
